d02d79c717cde00df6b8a4bda2dd62caccd87582
Cornelius Okeke <okekecornelius@gmail.com>
2020-03-19 11:51:50 +0100

    navigation systm upgrade

--- src/app/patient/patient-details/patient-details.component.ts
+++ src/app/patient/patient-details/patient-details.component.ts
@@ -3,6 +3,7 @@ import { IPatient } from 'src/app/interfaces/patient';
 import { ParamMap, ActivatedRoute, Router } from '@angular/router';
 import { PatientService } from '../patient.service';
 import { IPrescription } from 'src/app/interfaces/prescription';
+import { DialogService } from 'src/app/shared/service/dialog.service';
 
 @Component({
   selector: 'app-patient-details',
@@ -19,15 +20,20 @@ export class PatientDetailsComponent implements OnInit {
   constructor(
     public patientService: PatientService,
     public route: ActivatedRoute,
+    public dialogService: DialogService,
     private router: Router,
   ) { }
+
+
+  onDeleteDialog(prescriptionId: string) {
+    this.dialogService.prescriptionDeleteDialog(prescriptionId);
+  }
 
   initContents() {
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       this.patientId = paramMap.get('patientId');
       this.patientService.getPatientDetails(this.patientId)
         .subscribe((patientData: { patient: IPatient, prescriptions: IPrescription[] }) => {
-          console.log(patientData)
           this.patient = patientData.patient
           this.prescriptions = patientData.prescriptions;
           this.totalPrescriptions = this.prescriptions.length;
